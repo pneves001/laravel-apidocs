22c788c591a2e6726a782da2457d524bc268229b
Philip Neves <philip.neves@gmail.com>
2026-07-17 03:49:13 -0700

    fixing merged content.

--- resources/js/components/resource/Responses.vue
+++ resources/js/components/resource/Responses.vue
@@ -9,11 +9,7 @@
                     class="tab"
                     :class="{'current': index == current}"
                     @click="select(index)" >
-<<<<<<< HEAD
                     {{ item.code }} <span v-if="item.description" class="text-xs ml-1 opacity-50">({{ item.description }})</span>
-=======
-                    {{ item.description || index }}
->>>>>>> feature/response-descriptions
                 </div>
             </vue-custom-scrollbar>
 
